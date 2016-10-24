e033bee5825fbf585c659bc26776e1058103ff0c
jatin balodhi <jatinbalodhi@gmail.com>
2016-10-25 02:04:23 +0530

    'recent'

--- app/src/main/java/com/codemybackend/facility/ResultActivityState.java
+++ app/src/main/java/com/codemybackend/facility/ResultActivityState.java
@@ -12,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -174,6 +175,7 @@ public class ResultActivityState extends AppCompatActivity implements GoogleApiC
         my_latitude = lastlocation.getLatitude();
         my_longitude = lastlocation.getLongitude();
 
+        Log.d("mylat", my_latitude.toString());
     }
 
     @Override
